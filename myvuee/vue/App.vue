4f2ec4af0236af00d6577a095ed5f8bee83ab5df
www.com.git <sun_lifesky@163.com>
2020-11-03 11:32:09 +0800

    vue2020-11-03

---
+++ myvuee/vue/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="/Login">登录</router-link>
+        <router-link to="/Register">注册</router-link>
+        <router-view></router-view>    
+    </div>
+</template>
+
+<script>
+
+</script>
+
+
+<style scoped>
+    
+</style>
