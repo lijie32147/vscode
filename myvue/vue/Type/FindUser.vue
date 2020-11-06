41ed06969b0e1782674f3aed46fd304523c0c7ba
www.com.git <sun_lifesky@163.com>
2020-11-06 17:03:28 +0800

    vue项目练习2020-11-06

--- myvue/vue/Type/FindUser.vue
+++ myvue/vue/Type/FindUser.vue
@@ -3,20 +3,26 @@
         用户信息查询
         <table border="1" class="table table-hover">
             <tr>
-                <td>用户名</td>
-                <td>密码</td>
-                <td>操作</td>
+                <td width="40%">用户名</td>
+                <td width="40%">密码</td>
+                <td width="20%">操作</td>
             </tr>
             <tr v-for="item in list" :key="item.uid">
                 <td>{{item.uname}}</td>
                 <td>{{item.upassword}}</td>
-                <button>
+                <td>
                     <button class="btn btn-primary" @click = "del1(item.uid)">删除</button>
                     |
-                    <button class="btn btn-primary" @click = "update">修改</button>
-                </button>
+                    <button class="btn btn-primary" @click = "upd(item)">修改</button>
+                </td>
             </tr>
         </table>
+
+        <div id="u1" style="display:none">
+            用户名：<input type="text" v-model="uname"> <br>
+            密码：<input type="text" v-model="upassword"> <br>
+            <button type="button" @click="doupd()">修改</button>
+        </div>
     </div>
 </template>
 
@@ -25,6 +31,9 @@
     export default{
         data(){
             return{
+                uid: '',
+                uname: '',
+                upassword: '',
                 list: []
             }
         },
@@ -65,8 +74,30 @@
                 });
             },
 
-            update(){
-                console.log("update.........");
+            upd(item){
+
+                this.uid = item.uid;
+                this.uname = item.uname;
+                this.upassword = item.upassword;
+
+                layer.open({
+                    type: 1,
+                    skin: 'layui-layer-rim',
+                    area: ['420px', '240px'],
+                    content: $("#u1")
+                });
+            },
+
+            doupd(){
+
+                let data = {uid: this.uid, uname: this.uname, upassword: this.upassword};
+                let self = this;
+
+                axios.post("/user/updateUser",data).then(function(){
+                    layer.msg("修改成功！！！");
+                    self.uname = self.upassword = "";
+                    self.updateTable();
+                });
             }
         }
     }
